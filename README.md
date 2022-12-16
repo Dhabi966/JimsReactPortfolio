505795ebd60014aef628e0eef934e565d11ed8a5
Dhabi966 <jalewis012003@yahoo.com>
2022-12-15 23:09:26 -0700

    changes to readme

---
+++ README.md
@@ -0,0 +1,40 @@
+# React Portfolio
+
+## Description
+
+A website to demonstrate my knowledge as a web developer and a sneak peek of what I've worked on
+
+## Table Of Contents
+
+[Installation](#installation) <br>
+[Usage](#usage) <br>
+[Questions](#questions) <br>
+[Screenshot](#screenshot) <br>
+[Deployment](#deployment) <br>
+[Github](#github) <br>
+[Contribute](#contribute) <br>
+
+## Installation
+
+This application doesn't need to be installed. To see this portfolio, please visit the gitlink provided below.
+
+## Usage
+
+A portfolio for future employers to see my body of work
+
+## Questions
+
+https://github.com/Dhabi966
+
+
+## Screenshot
+
+
+
+## Deployment
+
+## GitHub
+
+## Contribute
+Feedback is always appreciated
+
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,36 +1,49 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function Contact() {
   return (
     <div>
       <h1>Contact Page</h1>
-    <div>
-    <Form>
+      <div>
+        <Form>
+          <Form.Group className="mb-3" controlId="formName">
+            <Form.Label>Full Name:</Form.Label>
+            <Form.Control type="name" placeholder="Name" />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formName">
-        <Form.Label>Full Name:</Form.Label>
-        <Form.Control type="name" placeholder="Name" />
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address:</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address:</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-        </Form.Text>
-      </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Your Message:</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
 
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Your Message:</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-    </div>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <div>
+          <a href="https://www.linkedin.com/in/james-lewis-897aa893/">
+            <i class="fab fa-linkedin"></i>
+          </a>
+        </div>
+        <div>
+          <a href="https://github.com/Dhabi966">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+        <div>
+        <a href="https://twitter.com/Diafollicle">
+            <i class="fab fa-twitter"></i>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
